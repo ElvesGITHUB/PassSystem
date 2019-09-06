9f42e22fcfe7a9fa8fd73d8163496d668a88733d
Elves <souzaelves@hotmail.com>
2019-09-05 22:48:00 -0300

    Início
    Novas pastas e arquivos

---
+++ comum.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/estilo.css">    
+    <title>Tela inicial</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Sua senha</h1>
+        <h2>Muito obrigado pela sua visita</h2>
+    </header>    
+    <main class="principal">
+        <div class="conteudo">
+          
+           <!--MODO ABSOLUTO-->
+            <?php 
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
+                
+        </div>
+    </main>
+    <footer class="rodape">
+        EBS  &copy <?= date('Y');?>
+    </footer>   
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/estilo.css">
+    <title>PassSystem</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>LogoLoja</h1>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo azul">
+                    <h3>Atendimento Digital</h3>
+                    <ul>
+                        <li>
+                            <a href="comum.php?dir=geral&file=senha">
+                            Gerar senha comum
+                            </a>
+                        </li>                                                                       
+                    </ul>
+                </div>     
+                <div class="modulo laranja">                    
+                    <ul>
+                        <li>
+                            <a href="comum.php?dir=prefer&file=senhaprefer">
+                            Gerar senha preferencial
+                            </a>
+                        </li>                                                                       
+                    </ul>
+                </div>     
+            </nav>        
+        </div>    
+    </main>
+</body>
+</html>
